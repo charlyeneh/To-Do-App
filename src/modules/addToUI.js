a98ee4c8b56878cd32fbd23c9fb74714a43d3406
Emperor_charly <charleschukwunweikeeneh@gmail.com>
2022-02-18 16:09:27 +0100

    Add the User interface module

---
+++ src/modules/addToUI.js
@@ -0,0 +1,62 @@
+import ToDo from "./todo.js";
+// Add items to UI
+const addtoList = () => {
+  const todoList = document.getElementById('todo-list');
+  todoList.innerHTML = '';
+
+  ToDo.list.forEach((item) => {
+    const listItem = document.createElement('li');
+    listItem.setAttribute('id', item.index);
+    listItem.classList = 'item-container';
+
+    listItem.innerHTML = `
+    <input class="checkbox" type="checkbox">
+    <input type="text" class="input" value='${item.description}'>
+    <p class="text-area">${item.description}</p>
+    <button class="cancel-btn"><i class="fa fa-trash" aria-hidden="true"></i></button>
+    `;
+
+    todoList.appendChild(listItem);
+
+    const checkbox = listItem.querySelector('input');
+    const text = listItem.querySelector('.input');
+    const textInput = listItem.querySelector('p');
+    const deleteButton = listItem.querySelector('button');
+
+    // Update
+    checkbox.addEventListener('change', () => {
+      const index = parseInt(listItem.id, 10);
+      ToDo.list[index].update();
+      text.classList.toggle('complete');
+      textInput.classList.toggle('complete');
+      localStorage.setItem('todoList', JSON.stringify(ToDo.list));
+    });
+
+        textInput.addEventListener('keydown', (e) => {
+      text.innerHTML = textInput.value;
+      const index = parseInt(listItem.id, 10);
+      ToDo.list[index].description = text.innerHTML;
+      localStorage.setItem('todoList', JSON.stringify(ToDo.list));
+      if (e.code === 'Enter') {
+        text.style.display = 'block';
+        textInput.classList.toggle('edit-item');
+      }
+    });
+
+    // Delete functionality
+    deleteButton.addEventListener('click', () => {
+      const index = parseInt(listItem.id, 10);
+      ToDo.list = ToDo.list.filter((item) => item !== ToDo.list[index]);
+      ToDo.list.forEach((item, i) => { item.index = i; });
+      localStorage.setItem('todoList', JSON.stringify(ToDo.list));
+      addtoList();
+    });
+
+    if (item.complete) {
+      checkbox.checked = true;
+      text.classList = 'complete';
+    }
+  });
+}
+
+    export default addtoList;
